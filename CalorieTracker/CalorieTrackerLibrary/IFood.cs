501023f355b646130bfff3fc743ef714c0ca0218
SandSide <37347875+SandSide@users.noreply.github.com>
2023-03-22 22:20:42 +0000

    Set up unit tests
    Set up unit tests
    Created food class

---
+++ CalorieTracker/CalorieTrackerLibrary/IFood.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieTrackerLibrary
+{
+    public interface IFood
+    {
+        string Name { get; set; }
+        int Calories { get; set; }
+
+    }
+}
