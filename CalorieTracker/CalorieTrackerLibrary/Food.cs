003a391eea2f16d59685a67ba4d9e7adf70bc659
SandSide <37347875+SandSide@users.noreply.github.com>
2023-03-22 21:04:59 +0000

    Set tup the project
    Created WPF solution, library solution and unit test solution

---
+++ CalorieTracker/CalorieTrackerLibrary/Food.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieTrackerLibrary
+{
+    internal interface Food
+    {
+        string Name { get; set; }
+        int calories { get; set; }
+
+    }
+}
