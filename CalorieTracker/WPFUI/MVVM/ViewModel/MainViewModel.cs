2ca8c7b6c87b921391c36c0e200f657610f2bf7c
SandSide <37347875+SandSide@users.noreply.github.com>
2023-04-07 03:01:41 +0100

    Setting up MVVM WPF

---
+++ CalorieTracker/WPFUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WPFUI.Core;
+
+namespace WPFUI.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand ChangeStringCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+		
+
+        private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private string testString;
+
+		public string TestString
+        {
+			get { return testString; }
+			set 
+			{ 
+				testString = value;
+                OnPropertyChanged();
+            }
+		}
+
+        public MainViewModel() 
+		{ 
+			HomeVM = new HomeViewModel();
+			CurrentView = HomeVM;
+
+            TestString = "Hello World!";
+
+			ChangeStringCommand = new RelayCommand(o => 
+			{
+				TestString = "Die";
+            });
+        }
+    }
+}
